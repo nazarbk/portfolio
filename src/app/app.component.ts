a289e3440788106036fa7f0c23c94b87e06ddec2
nazarbk <nazarblancokataran@gmail.com>
2023-09-19 13:41:35 +0200

    Repo Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  images = [
+    {
+      imageSrc: './assets/finn.png',
+      imageAlt: 'finn',
+    },
+    {
+      imageSrc: './assets/coraje.png',
+      imageAlt: 'coraje',
+    },
+    {
+      imageSrc: './assets/goku.png',
+      imageAlt: 'goku',
+    },
+    {
+      imageSrc: './assets/musculoso.png',
+      imageAlt: 'musculoso',
+    },
+    {
+      imageSrc: './assets/mario.png',
+      imageAlt: 'mario',
+    },
+    {
+      imageSrc: './assets/bojji.png',
+      imageAlt: 'bojji',
+    }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { IonicModule } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselModule } from './carousel/carousel.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    IonicModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
